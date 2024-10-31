af64044bacd95b83507b97447d2917200a7ccf30
dariusstone7 <stonefightx@gmail.com>
2024-10-31 18:29:09 +0100

    recherche et mis a jour des quantités

--- app/details.tsx
+++ app/details.tsx
@@ -1,4 +1,4 @@
-import { View, Text, TextInput, ScrollView, StyleSheet, FlatList, Pressable, TouchableOpacity, Dimensions, Modal, } from "react-native";
+import { View, Text, TextInput, ScrollView, StyleSheet, FlatList, Pressable, TouchableOpacity, Dimensions, Modal, KeyboardAvoidingView, Platform} from "react-native";
 import { useLocalSearchParams } from "expo-router";
 import * as FileSystem from 'expo-file-system';
 import { useState, useEffect } from "react";
@@ -8,117 +8,137 @@ import { RefreshControl } from "react-native-gesture-handler";
 import modal from "@/components/modal";
 import { Ionicons } from "@expo/vector-icons";
 
+
 export default function DetailsScreen(){
 
     const { fileData } = useLocalSearchParams();
     const [fileLines, setFileLines] = useState<string[]>([]);
     let [products, setProducts] = useState<Product[]>([]);
-    const [refreshing, setRefreshing] = useState<boolean>(false);
-    const [searchText, setSearchText] = useState<string>();
-    const [modalVisible, setModalVisible] = useState<boolean>(false);
-    const [selectedPoduct, setSelectedProduct] = useState<Product>();
-    const [selectedIndex, setSelectedIndex] = useState<Number>();
+    let [initialProducts, setInitialProducts] = useState<Product[]>([]);
+    let [refreshing, setRefreshing] = useState<boolean>(false);
+    let [searchText, setSearchText] = useState<string>();
+    let [modalVisible, setModalVisible] = useState<boolean>(false);
+    let [selectedPoduct, setSelectedProduct] = useState<Product>();
+    let [selectedIndex, setSelectedIndex] = useState<Number>();
 
     // let lines = (fileData.toString()).split("\n");
     // console.log(lines);
-    let initialProducts = [] as Product[];
+    // let initialProducts = [] as Product[]
 
     let lines = [
         'CDT1;LAIT BROLI CHOCOLOCO PULS TROIS 1KG(CARTON);15',
         'CDT2;BISCUIT NAYA 150G(PAQUET);4',
         'CDT3;EAU SUPERMONT 10L(BIDON);4',
-        'CDT1;LAIT BROLI 1KG(CARTON);23',
-        'CDT2;BISCUIT NAYA 150G(PAQUET);5',
-        'CDT3;EAU SUPERMONT 10L(BIDON);12',
-        'CDT1;LAIT BROLI 1KG(CARTON);23',
-        'CDT2;BISCUIT NAYA 150G(PAQUET);5',
-        'CDT3;EAU SUPERMONT 10L(BIDON);12',
-        'CDT1;LAIT BROLI 1KG(CARTON);23',
-        'CDT2;BISCUIT NAYA 150G(PAQUET);5',
-        'CDT3;EAU SUPERMONT 10L(BIDON);12',
-        'CDT1;LAIT BROLI 1KG(CARTON);23',
-        'CDT2;BISCUIT NAYA 150G(PAQUET);5',
-        'CDT3;EAU SUPERMONT 10L(BIDON);12',
+        'CDT4;LAIT BROLI 1KG(CARTON);23',
+        'CDT5;BISCUIT NAYA 150G(PAQUET);5',
+        'CDT6;EAU SUPERMONT 10L(BIDON);12',
+        'CDT7;LAIT BROLI 1KG(CARTON);23',
+        'CDT8;BISCUIT NAYA 150G(PAQUET);5',
+        'CDT9;EAU SUPERMONT 10L(BIDON);12',
+        'CDT10;LAIT BROLI 1KG(CARTON);23',
+        'CDT11;BISCUIT NAYA 150G(PAQUET);5',
+        'CDT12;EAU SUPERMONT 10L(BIDON);12',
+        'CDT13;LAIT BROLI 1KG(CARTON);23',
+        'CDT14;BISCUIT NAYA 150G(PAQUET);5',
+        'CDT15;EAU SUPERMONT 10L(BIDON);12',
     ];
 
-    lines.forEach((line, index)=>{
-        let lineSplit = line.split(";");
-        let namecdt = lineSplit[1].split('(');
-
-        let product = new Product(lineSplit[0], namecdt[0], '('+namecdt[1], Number(lineSplit[2]));
-
-        initialProducts[index] = new Product(lineSplit[0], namecdt[0], '('+namecdt[1], Number(lineSplit[2]));
-    });
-
-    // let initialProducts = [
-    //     new Product("CDT1", "LAIT BROLI CHOCOLOCO PULS TROIS 1KG", "CARTON", 15),
-    //     new Product("CDT2", "BISCUIT NAYA 150G", "PAQUET", 4),
-    //     new Product("CDT3", "EAU SUPERMONT 10L", "BIDON", 4),
-    //     new Product("CDT4", "LAIT BROLI 1KG", "CARTON", 4),
-    //     new Product("CDT5", "BISCUIT NAYA 150G", "PAQUET", 4),
-    //     new Product("CDT6", "EAU SUPERMONT 10L", "BIDON", 4),
-    //     new Product("CDT7", "LAIT BROLI 1KG", "CARTON", 4),
-    //     new Product("CDT8", "BISCUIT NAYA 150G", "PAQUET", 4),
-    //     new Product("CDT9", "EAU SUPERMONT 10L", "BIDON", 4),
-    //     new Product("CDT10", "LAIT BROLI 1KG", "CARTON", 4),
-    //     new Product("CDT11", "BISCUIT NAYA 150G", "PAQUET", 4),
-    //     new Product("CDT12", "EAU SUPERMONT 10L", "BIDON", 4),
-    //     new Product("CDT13", "LAIT BROLI 1KG", "CARTON", 4),
-    //     new Product("CDT14", "BISCUIT NAYA 150G", "PAQUET", 4),
-    //     new Product("CDT15", "EAU SUPERMONT 10L", "BIDON", 4),
-    // ];
 
     //initialisation de la liste de produits lorsque le composant est monté
-    useEffect(() => {
-        setProducts(initialProducts);
-        setModalVisible(false);
+    useEffect( () => {
+
+        let productsList = initProducts(lines);
+        setInitialProducts(productsList);
+        setProducts(productsList);
+
     }, []);
 
+
     //Rechargement de la liste des produits lorsqu'on recharge l'écran
-    const onRefresh = ()=>{
+    const onRefresh = () => {
+
         setRefreshing(true);
-        setProducts(initialProducts);
-        setModalVisible(false);
-        setTimeout(()=>{setRefreshing(false)}, 1000);
+        setInitialProducts(products);
+        setTimeout(()=>{setRefreshing(false)}, 500);
+
     }
+
+
+    //Formatage du contenu du fichier en un tableau de produit
+    const initProducts = (lines : String[]) => {
+
+        let products = [] as Product[];
+
+        lines.forEach( (line, index) => {
+
+            let lineSplit = line.split(";");
+            let namecdt = lineSplit[1].split('(');
+            let product = new Product(lineSplit[0], namecdt[0], '('+namecdt[1], Number(lineSplit[2]));
+    
+            products.push(product);
+
+        });
+
+        return products;
+
+    }
+
 
     //Filtre de la liste des produits en fonction de la valeur de recherche saisie
-    const filterProducts = (key:string)=>{
-        console.log(key);
-        let result = initialProducts.filter((product)=>{
+    const filterProducts = (key:string) => {
+     
+        if(key.length === 0){
+            setInitialProducts(products);
+            return;
+        }
+
+        let result = initialProducts.filter( (product) => {
             return (product.getName()).includes(key.toUpperCase());
         });
-        setProducts(result);
-        console.log(products);
+       
+        setInitialProducts(result);
     }
 
+
+    //Mis à jour de la quantité du produit selectionné avec la nouvelle valeur saisir
     const updateQuantity = (quantity: string) => {
 
-        selectedPoduct?.setQuantity(Number(quantity));
-        setSelectedProduct(selectedPoduct);
+        let product = new Product(selectedPoduct?.getId(), selectedPoduct?.getName(), selectedPoduct?.getCondtionment(), selectedPoduct?.getQuantity());
+
+        product?.setQuantity(Number(quantity));
+
+        setSelectedProduct(product);
 
         initialProducts[Number(selectedIndex)]?.setQuantity(Number(quantity));
+        setInitialProducts(initialProducts);
         setProducts(initialProducts);
 
     }
 
+
+    //Traitement lors de l'ouverture du modal
     const openModal = (product : Product, index: Number) => {
+
         setSelectedProduct(product);
         setSelectedIndex(index);
-
         setModalVisible(true);
 
         return null;
     }
 
-    const closeModal = ()=> {
+
+    //Traitement lors de la cloture du modal
+    const closeModal = () => {
+
         setModalVisible(false);
-        console.log(products[Number(selectedIndex)]);
+        console.log('Produit mis à jour: ' + products[Number(selectedIndex)]);
 
     }
 
+
+    //Vue à afficher à l'écran
     return (
-        <View style={{paddingVertical: 20, paddingHorizontal:10, backgroundColor: "#eff5f7",}}>
+        <View style={ {paddingVertical: 20, paddingHorizontal:10, backgroundColor: "#eff5f7",} }>
             <TextInput 
                 value={searchText}
                 placeholder="Rechercher"
@@ -133,39 +153,34 @@ export default function DetailsScreen(){
             </TextInput>
 
             <FlatList
-                data={products} 
+                data={initialProducts} 
                 renderItem={ 
                     ({item, index}) => <Text onPress={()=>{openModal(item, index)}} style={styles.productCard}> <ProductComponent product={item}/> </Text>
                 }
                 keyExtractor={(item) => item.getId()}
-                style={styles.flatlist}
                 contentContainerStyle = {styles.flatlistContainer}
                 refreshing={refreshing}
                 onRefresh={onRefresh}
-                // refreshControl={
-                //     <RefreshControl 
-                //         refreshing={refreshing} 
-                //         onRefresh={onRefresh} 
-                //         title="Chargement..."
-                //         tintColor="red"
-                //     />
-                // }
-                showsVerticalScrollIndicator={false}
-
-                ListFooterComponent={()=><Text>Nombre de Produits: {products.length}</Text>}
-
-                ListEmptyComponent={()=><Text style={{color: "#ff9900"}}>Aucun produit trouvé !</Text>}
+                style={styles.flatlist}
+                showsVerticalScrollIndicator={ false } 
+                ListFooterComponent={ () => <Text> Nombre de Produits: { products.length } </Text> }
+                ListEmptyComponent={ () => <Text style={ {color: "#ff9900"} }> Aucun produit trouvé ! </Text> }
                 
             >
             </FlatList>
 
-            <Modal transparent={true} animationType="slide" visible={modalVisible} onPointerLeave={()=>setModalVisible(false)}>
-                <View style={styles.modal}>
-                    <Ionicons name="close-circle" size={48} color="#fff" onPress={closeModal} style={styles.closeButton}></Ionicons>
-                    <Text> {selectedPoduct?.getName()} </Text>
-                    <Text> {selectedPoduct?.getCondtionment()} </Text>
-                    <TextInput placeholder="Quantité" value={(selectedPoduct?.getQuantity())?.toString()} onChangeText={(text)=>{updateQuantity(text)}} selectTextOnFocus keyboardType="numeric" style={styles.input} ></TextInput>
-                </View>
+            <Modal transparent={ true } animationType="slide" visible={ modalVisible } onPointerLeave={ () => setModalVisible(false) }>
+                <KeyboardAvoidingView style={ {flex:1, justifyContent:"flex-end"} }>
+                    <TouchableOpacity onPress={ closeModal } style={ styles.modalOverlay } activeOpacity={ 1 }>
+                        <TouchableOpacity style={ styles.modal } activeOpacity={ 1 }>
+                            <Ionicons name="checkmark-circle-outline" size={ 48 } color="#02c4ba" onPress={ closeModal } style={ styles.closeButton }></Ionicons>
+                            <Text style={ {fontSize:20, color: "#0000009b", fontWeight: "bold"} }> { selectedPoduct?.getName() } </Text>
+                            <Text style={ {fontSize:14, color: "#0000009b",} }> { selectedPoduct?.getCondtionment() } </Text>
+                            <Text style={ {fontSize:14, color: "#0000009b", marginTop: 20,} }> Quantité: </Text>
+                            <TextInput onBlur={ closeModal } placeholder="Quantité" value={ (selectedPoduct?.getQuantity())?.toString() } onChangeText={ (text) => {updateQuantity(text)} } selectTextOnFocus keyboardType="numeric" style={ styles.input } />
+                        </TouchableOpacity>
+                    </TouchableOpacity>
+                </KeyboardAvoidingView>
             </Modal>
 
         </View>
@@ -173,6 +188,8 @@ export default function DetailsScreen(){
     )
 }
 
+
+//définition des styles
 const styles = StyleSheet.create({
     flatlist:{
       marginVertical: 0,
@@ -198,13 +215,19 @@ const styles = StyleSheet.create({
     modal:{
        backgroundColor: "#dfeaee",
        width: "100%",
-       height: "30%",
+       height: "35%",
        position: "absolute",
        bottom: 0,
        borderTopRightRadius: 25,
        borderTopLeftRadius: 25,
        paddingHorizontal: 20,
-       paddingVertical: 30,
+       paddingVertical: 50,
+       
+    },
+    modalOverlay:{
+        flex: 1,
+        justifyContent: "flex-end",
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
     },
     closeButton:{
         position: "absolute",
@@ -215,8 +238,8 @@ const styles = StyleSheet.create({
         height: 50,
         width: "100%",
         backgroundColor: "#fff",
-        marginTop: 10,
         paddingHorizontal: 10,
         borderRadius: 10,
+        marginTop: 4,
     }
 })
